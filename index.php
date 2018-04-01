12768b5ebc3aabcbdfad4ec6a2b13c6a0a31498a
Mac32 <alberth.bompart22@gmail.com>
2018-04-01 11:43:10 -0400

    Agregado de archivos: Jquery, Normalize, style y script.

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 
+// Se incializa la sesión
 session_start();
-if (isset($_SESSION["login"])){
-    header("location:home");
+if (isset($_SESSION["login"])) {
+    header("location:home.php");
 }
 
 ?>
@@ -12,7 +13,10 @@ if (isset($_SESSION["login"])){
 <head lang="es">
 <meta charset="utf-8" />
 <title>Mi Libreria</title>
-<link rel="stylesheet" type="text/css"  href="css/style.css" />
+<link rel="stylesheet" type="text/css" href="css/Normalize.css" />
+<link rel="stylesheet" type="text/css" href="css/style.css" />
+<script type="text/javascript" src="js/jquery-3.3.1.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
 </head>
 <body>
 
